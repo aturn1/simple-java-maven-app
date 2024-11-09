35a268581c167c05246c55b24dfdd361f0cad320
aturn1 <naresh.batch001@gmail.com>
2024-11-09 17:25:32 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
     }
-/*comment*/
+/*comment*//*comment*/
     public String getMessage() {
         return MESSAGE;
     }
