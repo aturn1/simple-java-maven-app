5d55ae63ecfd54aa904f1124286d6e3745bf9bdf
aturn1 <naresh.batch001@gmail.com>
2024-11-09 11:13:11 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
-        for (i=1;i<=10;)
+        for (int i=1;i<=10;)
             {
                 System.out.println(i);
             }
